f6807dfc63fc2e5ea33318718c3cd1139d0ff50f
SilentCA <2291948161@qq.com>
2023-12-17 12:06:56 +0800

    修改界面使得界面能够伸展 添加图标

--- Invoice2ExcelApp.onefile.spec
+++ Invoice2ExcelApp.onefile.spec
@@ -9,7 +9,8 @@ hidden_imports = [
         ]
 
 data_files = [
-        ('Invoice2ExcelApp.ui','.')
+        ('Invoice2ExcelApp.ui','.'),
+        ('icon.png','.')
         ]
 
 a = Analysis(['Invoice2ExcelApp.py'],
@@ -45,4 +46,5 @@ exe = EXE(pyz,
           disable_windowed_traceback=False,
           target_arch=None,
           codesign_identity=None,
-          entitlements_file=None )
+          entitlements_file=None,
+          icon='icon.ico' )
--- Invoice2ExcelApp.py
+++ Invoice2ExcelApp.py
@@ -1,11 +1,15 @@
 import pathlib
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import pygubu
 
 import logging
+import pathlib
 import Invoice2Excel
+import pandas as pd
+import openpyxl
+import os
 
 
 # Configure logging
@@ -50,7 +54,9 @@ class Invoice2ExcelApp:
         self.in_path = tk.StringVar()
         self.out_path = tk.StringVar()
         self.is_mode_append = tk.BooleanVar()
-        builder.import_variables(self, ['in_path', 'out_path', 'is_mode_append'])
+        self.filenamesVar = tk.StringVar()
+        self.filenum = tk.IntVar()
+        builder.import_variables(self, ['in_path', 'out_path', 'is_mode_append', 'filenamesVar', 'filenum'])
 
         builder.connect_callbacks(self)
         
@@ -65,6 +71,14 @@ class Invoice2ExcelApp:
     def on_button_select_in_path_clicked(self):
         dirname = filedialog.askdirectory()
         self.in_path.set(dirname)
+        filenames = list(pathlib.Path(dirname).rglob('*.pdf')) 
+        self.filenamesVar.set(filenames)
+        self.filenum.set(len(filenames))
+
+    def on_button_select_in_files_clicked(self):
+        filenames = filedialog.askopenfilenames(filetypes=[('PDF','*.pdf')])
+        self.filenamesVar.set(filenames)
+        self.filenum.set(len(filenames))
 
     def on_button_select_out_path_clicked(self):
         filepath = filedialog.asksaveasfilename(filetypes=[('Excel','*.xlsx')], defaultextension='xlsx')
@@ -75,10 +89,43 @@ class Invoice2ExcelApp:
         in_path = self.in_path.get()
         out_path = self.out_path.get()
         logger.info("处理开始")
-        logger.info(f"附加模式: {is_mode_append}\n发票目录: {in_path}\n保存路径: {out_path}")
+        logger.info(f"\n\t附加模式: {is_mode_append}\n\t发票目录: {in_path}\n\t保存路径: {out_path}")
+        logger.info('正在处理中请稍后...')
         write_mode = 'append' if is_mode_append else 'write'
-        Invoice2Excel.invoice2Excel(in_path, out_path, write_mode)
+        self.invoice2excel(eval(self.filenamesVar.get()), out_path, write_mode)
         logger.info('处理结束\n')
+        messagebox.showinfo(message='处理完成')
+
+    def invoice2excel(self, filenames:list, out_path:str, write_mode:str):
+        num = len(filenames)
+        logger.info(f'Total files to parse: {num}.')
+        data = pd.DataFrame()
+        for idx, f in enumerate(filenames):
+            logger.info(f'{idx+1}/{num}({round((idx+1)/num*100)}%)\t{f}')
+            extractor = Invoice2Excel.Extractor(f)
+            try:
+                d = extractor.extract()
+                df_item = pd.DataFrame()
+                df_item.loc[0, '发票文件'] = f'=HYPERLINK("{os.path.abspath(f)}","{f}")'
+                d = pd.concat([d, df_item], axis=1)
+                data = pd.concat([data, d], axis=0, sort=False, ignore_index=True)
+            except Exception as e:
+                logger.error('File error:', f, '\n', e)
+            self.builder.get_object('progressbar1').step(100/num)
+
+        logger.info(f'Finish parsing, save data to {out_path}')
+        if write_mode == 'write':
+            data.to_excel(out_path, sheet_name='data')
+        elif write_mode == 'append':
+            wb = openpyxl.load_workbook(out_path)
+            ws = wb['data']
+            # skip header and a blank row next to header
+            rows = openpyxl.utils.dataframe.dataframe_to_rows(data, header=False)
+            rows.__next__()
+            for r in rows:
+                ws.append(r)
+            wb.save(out_path)
+        logger.info(f'All Done.')
 
 
 if __name__ == '__main__':
